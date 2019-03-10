ea1220ea88763cef10f23bff85a86f3c0d86e324
Vladimir Ermakov <vooon341@gmail.com>
2019-03-10 23:17:57 +0300

    try to use fresh GATT LED example

--- src/PowerMeterService.h
+++ src/PowerMeterService.h
@@ -1,22 +1,35 @@
 
 #pragma once
 
+#include <array>
+
 #include "ble/BLE.h"
 #include "ble/UUID.h"
 
 class PowerMeterService {
     public:
-	const UUID UUID_POWER_METER_SERVICE = "caf92d82-4353-11e9-949b-4ccc6ad67d53";
+	const UUID UUID_CHAR_PRESENTATION_FORMAT = 0x2904;
+
+	static const UUID UUID_POWER_METER_SERVICE; // "caf92d82-4353-11e9-949b-4ccc6ad67d53";
 	const UUID UUID_POWER_COUNTER_CHAR = 0xb001;
 	const UUID UUID_PULSE_COUNTER_CHAR = 0xb002;
 	const UUID UUID_PULSES_PER_KWH_CHAR = 0xb003;
 
 	PowerMeterService(BLEDevice& _ble, uint32_t init_power_ctr, uint32_t init_pulse_ctr, uint16_t init_pulse_per_kwh)
 	    : ble(_ble)
-	    , power_counter(UUID_POWER_COUNTER_CHAR, &init_power_ctr, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, nullptr, 0)
-	    , pulse_counter(UUID_PULSE_COUNTER_CHAR, &init_pulse_ctr, 0, nullptr, 0)
-	    , pulses_per_kwh(UUID_PULSES_PER_KWH_CHAR, &init_pulse_per_kwh, 0, nullptr, 0)
+	    //, power_counter(UUID_POWER_COUNTER_CHAR, &init_power_ctr, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, power_attrs.data(), power_attrs.size())
+	    //, pulse_counter(UUID_PULSE_COUNTER_CHAR, &init_pulse_ctr, 0, pulse_attrs.data(), pulse_attrs.size())
+	    //, pulses_per_kwh(UUID_PULSES_PER_KWH_CHAR, &init_pulse_per_kwh, 0, pulses_per_kwh_attrs.data(), pulses_per_kwh_attrs.size())
+	    , power_counter(UUID_POWER_COUNTER_CHAR, &init_power_ctr, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY)
+	    , pulse_counter(UUID_PULSE_COUNTER_CHAR, &init_pulse_ctr, 0)
+	    , pulses_per_kwh(UUID_PULSES_PER_KWH_CHAR, &init_pulse_per_kwh, 0)
+
 	{
+		std::array<GattCharacteristic*, 3> char_table { { &power_counter, &pulse_counter, &pulses_per_kwh } };
+
+		GattService service(UUID_POWER_METER_SERVICE, char_table.data(), char_table.size());
+
+		ble.gattServer().addService(service);
 	}
 
     private:
@@ -26,7 +39,7 @@ class PowerMeterService {
 	ReadWriteGattCharacteristic<uint32_t> pulse_counter;
 	ReadWriteGattCharacteristic<uint16_t> pulses_per_kwh;
 
-	const GattCharacteristic::PresentationFormat_t power_presentation {
+	GattCharacteristic::PresentationFormat_t power_presentation {
 		.gatt_format = GattCharacteristic::BLE_GATT_FORMAT_UINT32,
 		.exponent = -3,
 		.gatt_unit = GattCharacteristic::BLE_GATT_UNIT_ENERGY_KILOWATT_HOUR,
@@ -34,7 +47,7 @@ class PowerMeterService {
 		.gatt_nsdesc = 0x0000
 	};
 
-	const GattCharacteristic::PresentationFormat_t pulse_presentation {
+	GattCharacteristic::PresentationFormat_t pulse_presentation {
 		.gatt_format = GattCharacteristic::BLE_GATT_FORMAT_UINT32,
 		.exponent = 0,
 		.gatt_unit = GattCharacteristic::BLE_GATT_UNIT_NONE,
@@ -43,7 +56,7 @@ class PowerMeterService {
 
 	};
 
-	const GattCharacteristic::PresentationFormat_t pulses_per_kwh_presentation {
+	GattCharacteristic::PresentationFormat_t pulses_per_kwh_presentation {
 		.gatt_format = GattCharacteristic::BLE_GATT_FORMAT_UINT16,
 		.exponent = 0,
 		.gatt_unit = GattCharacteristic::BLE_GATT_UNIT_NONE,
@@ -52,7 +65,15 @@ class PowerMeterService {
 
 	};
 
-	//GattAttribute power_attrs[1];
-	//GattAttribute pulse_attrs[1];
-	//GattAttribute pulses_per_kwh_attrs[1];
+	static constexpr size_t PF_SIZE = sizeof(GattCharacteristic::PresentationFormat_t);
+
+	GattAttribute power_pr_attr { UUID_CHAR_PRESENTATION_FORMAT, reinterpret_cast<uint8_t*>(&power_presentation), PF_SIZE, PF_SIZE };
+	GattAttribute pulse_pr_attr { UUID_CHAR_PRESENTATION_FORMAT, reinterpret_cast<uint8_t*>(&pulse_presentation), PF_SIZE, PF_SIZE };
+	GattAttribute pulses_per_kwh_pr_attr { UUID_CHAR_PRESENTATION_FORMAT, reinterpret_cast<uint8_t*>(&pulses_per_kwh_presentation), PF_SIZE, PF_SIZE };
+
+	std::array<GattAttribute*, 1> power_attrs { { &power_pr_attr } };
+	std::array<GattAttribute*, 1> pulse_attrs { { &pulse_pr_attr } };
+	std::array<GattAttribute*, 1> pulses_per_kwh_attrs { { &pulses_per_kwh_pr_attr } };
 };
+
+const UUID PowerMeterService::UUID_POWER_METER_SERVICE { { "caf92d82-4353-11e9-949b-4ccc6ad67d53" } };
--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,9 @@ I2C i2c_bmi_bmp(P0_11, P0_12);
 
 BME280 bme(i2c_ap_bme);
 
-const char DEVICE_NAME[] = "Power Meter";
-const uint16_t uuid16_list[] = { GattService::UUID_ENVIRONMENTAL_SERVICE };
+const char DEVICE_NAME[] = "Power-Meter";
 
-// Queue
-EventQueue event_queue;
-EnvironmentalService* penvsrv = nullptr;
+EventQueue event_queue(16 * EVENTS_EVENT_SIZE);
 
 void next_color()
 {
@@ -55,52 +52,126 @@ void next_color()
 		it = colors.cbegin();
 }
 
-void disconnection_cb(const Gap::DisconnectionCallbackParams_t* params)
-{
-	// restart advertising
-	BLE::Instance().gap().startAdvertising();
-}
-
-void ble_init_complete(BLE::InitializationCompleteCallbackContext* params)
-{
-	BLE& ble = params->ble;
-	auto& gap = ble.gap();
-
-	//auto error = params->error;
-
-	penvsrv = new EnvironmentalService(ble);
-	gap.onDisconnection(disconnection_cb);
-
-	// Setup advertising
-	gap.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
-	gap.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t*)uuid16_list, sizeof(uuid16_list));
-	gap.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t*)DEVICE_NAME, sizeof(DEVICE_NAME));
-	gap.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
-	gap.setAdvertisingInterval(1000);
-
-	gap.startAdvertising();
-}
-
 void read_sensor()
 {
-	float t, p, h;
-
-	if (penvsrv == nullptr)
-		return;
-
-	t = bme.getTemperature();
-	p = bme.getPressure();
-	h = bme.getHumidity();
-
-	penvsrv->updateTemperature(t);
-	penvsrv->updatePressure(p);
-	penvsrv->updateHumidity(h);
 }
 
-void sched_ble_events_processing(BLE::OnEventsToProcessCallbackContext* context)
+class PowerMeter : ble::Gap::EventHandler {
+    public:
+	PowerMeter(BLE& _ble, EventQueue& _evq)
+	    : ble(_ble)
+	    , evq(_evq)
+	    , _pm_service(nullptr)
+	    , _adv_data_builder(_adv_buffer)
+	{
+	}
+
+	void start()
+	{
+		ble.gap().setEventHandler(this);
+		ble.init(this, &PowerMeter::on_init_complete);
+
+		//evq.call_every(500, this, &LEDDemo::blink);
+
+		evq.dispatch_forever();
+	}
+
+    private:
+	/// Callback triggered when the ble initialization process has finished
+	void on_init_complete(BLE::InitializationCompleteCallbackContext* params)
+	{
+		if (params->error != BLE_ERROR_NONE) {
+			//printf("Ble initialization failed.");
+			return;
+		}
+
+		_pm_service = new PowerMeterService(ble, 0, 0, 3200);
+
+		ble.gattServer().onDataWritten(this, &PowerMeter::on_data_written);
+
+		//print_mac_address();
+
+		start_advertising();
+	}
+
+	void start_advertising()
+	{
+		/* Create advertising parameters and payload */
+
+		ble::AdvertisingParameters adv_parameters(
+		    ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
+		    ble::adv_interval_t(ble::millisecond_t(1000)));
+
+		_adv_data_builder.setFlags();
+		_adv_data_builder.setLocalServiceList(mbed::make_Span(svc_uuids.data(), svc_uuids.size()));
+		_adv_data_builder.setName(DEVICE_NAME);
+
+		/* Setup advertising */
+
+		auto& gap = ble.gap();
+		ble_error_t error = gap.setAdvertisingParameters(
+		    ble::LEGACY_ADVERTISING_HANDLE,
+		    adv_parameters);
+
+		if (error) {
+			//printf("gap.setAdvertisingParameters() failed\r\n");
+			return;
+		}
+
+		error = gap.setAdvertisingPayload(
+		    ble::LEGACY_ADVERTISING_HANDLE,
+		    _adv_data_builder.getAdvertisingData());
+
+		if (error) {
+			//printf("gap.setAdvertisingPayload() failed\r\n");
+			return;
+		}
+
+		/* Start advertising */
+
+		error = gap.startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+
+		if (error) {
+			//printf("gap.startAdvertising() failed\r\n");
+			return;
+		}
+	}
+
+	/**
+	 * This callback allows the PowerMeterService to receive updates to the Characteristic.
+	 *
+	 * @param[in] params Information about the characterisitc being updated.
+	 */
+	void on_data_written(const GattWriteCallbackParams* params)
+	{
+		//if ((params->handle == _led_service->getValueHandle()) && (params->len == 1)) {
+		//	_actuated_led = *(params->data);
+		//}
+	}
+
+    private:
+	/* Event handler */
+
+	void onDisconnectionComplete(const ble::DisconnectionCompleteEvent&)
+	{
+		ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+	}
+
+    private:
+	BLE& ble;
+	events::EventQueue& evq;
+
+	const std::array<UUID, 1> svc_uuids { { PowerMeterService::UUID_POWER_METER_SERVICE } };
+
+	PowerMeterService* _pm_service;
+
+	uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
+	ble::AdvertisingDataBuilder _adv_data_builder;
+};
+
+void sched_ble_events(BLE::OnEventsToProcessCallbackContext* context)
 {
-	auto& ble = BLE::Instance();
-	event_queue.call(Callback<void()>(&ble, &BLE::processEvents));
+	event_queue.call(Callback<void()>(&context->ble, &BLE::processEvents));
 }
 
 int main()
@@ -110,13 +181,13 @@ int main()
 	bme.initialize();
 
 	auto& ble = BLE::Instance();
-	ble.onEventsToProcess(sched_ble_events_processing);
-	ble.init(ble_init_complete);
+	ble.onEventsToProcess(sched_ble_events);
 
 	event_queue.call_every(1000, next_color);
 	event_queue.call_every(1000, read_sensor);
 
-	event_queue.dispatch_forever();
+	PowerMeter pm(ble, event_queue);
+	pm.start();
 
 	return 0;
 }
